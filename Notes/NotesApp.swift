f0f727547a9191c934d4c05e1d2261a9f96803f5
Sergei Semenov <ssemenov@me.com>
2024-11-14 15:26:07 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Sergei Semenov on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
